4bda4256dc629efaa9e52e19ed1ec18e7001ec7c
Zack Paul <zkpaul@outlook.com>
2025-04-26 09:44:06 -0600

    set homepage

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.sendFile(_dirName + '/index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(PORT, () => {
