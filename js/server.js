a4c5900152770632992e076bc9757a8e7ff73dbc
Zack Paul <zkpaul@outlook.com>
2025-04-26 09:37:49 -0600

    set up server functionality

--- js/server.js
+++ js/server.js
@@ -1,10 +1,14 @@
 const express = require ('express');
 const app = express();
-const port = 8000;
+const PORT = 8000;
 const cors = require('cors');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(_dirName + '/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
 })
