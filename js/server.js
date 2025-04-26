bca6ea1970b50aa5cba7fe003a504f5799f23ce8
Zack Paul <zkpaul@outlook.com>
2025-04-26 09:36:05 -0600

    set up homepage path

--- js/server.js
+++ js/server.js
@@ -2,3 +2,9 @@ const express = require ('express');
 const app = express();
 const port = 8000;
 const cors = require('cors');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.sendFile(_dirName + '/index.html')
+})
