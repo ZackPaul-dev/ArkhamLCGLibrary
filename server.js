2159f192bbb902fb191a674dd998ef048b9e9ac7
Zack Paul <zkpaul@outlook.com>
2025-06-10 15:52:00 -0600

    troubleshooting

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ fetch(`https://arkhamdb.com/api/public/cards?_format=json`)
 .then(data => {
     console.log(data)
 })
-.then(data => {
-   let collection = data;
-   res.write(collection)
-})
 .catch(err =>{
     console.log(`error${err}`)
 })
