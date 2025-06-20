ef2f2b0c0bbf2f4665b7c7fa4a9139cdbdbbbb1b
Zack Paul <zkpaul@outlook.com>
2025-06-20 15:00:02 -0600

    corrected fetch pass from server

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ document.querySelector('button').addEventListener('click', findGator)
 async function getCards(req, res){
 
         fetch('cards')
-        .then(res => res.json)
+        .then(res => res.json())
         .then(data => console.log(data))
 };
 
 getCards();
 
-/*async function findGator(){
+async function findGator(){
     const gatorName = document.querySelector('input').values
    
         document.querySelector('h2').innerText = collection.real_name
-    } */
+    } 
