6b988b87a0d3ac044f74d3f7dd5a87d8d72c55a8
Zack Paul <zkpaul@outlook.com>
2025-06-20 14:57:07 -0600

    working

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@ alert('working');
 
 document.querySelector('button').addEventListener('click', findGator)
 
-async function getCards(){
-let collection = await fetch('cards')
-console.log(collection)
+async function getCards(req, res){
+
+        fetch('cards')
+        .then(res => res.json)
+        .then(data => console.log(data))
 };
 
 getCards();
 
-async function findGator(){
+/*async function findGator(){
     const gatorName = document.querySelector('input').values
    
         document.querySelector('h2').innerText = collection.real_name
-    } 
+    } */
