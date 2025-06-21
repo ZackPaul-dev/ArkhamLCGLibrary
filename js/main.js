35268edd3386aeb2b68d7f4a79080718956a5367
Zack Paul <zkpaul@outlook.com>
2025-06-21 11:35:12 -0600

    working on network issues

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ async function getCards(req, res){
 
         fetch('cards')
         .then(res => res.json())
-        //.then(data => console.log(data))
-        .then(data => {
+        .then(data => console.log(data))
+        /*.then(data => {
    collection = data;
    console.log(collection)
-});
+});*/
 }
 getCards();
 
