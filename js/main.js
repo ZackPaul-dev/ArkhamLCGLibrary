3bc4c0ddf671e5fb510720b1e9293e00e3ad11f7
Zack Paul <zkpaul@outlook.com>
2025-08-02 10:56:17 -0600

    added API pull for card art on search

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-
-alert('working');
-
 let collection = undefined;
 
 document.querySelector('button').addEventListener('click', findGator)
@@ -28,6 +25,7 @@ async function findGator(){
     let cardDesc = collection.find((obj) => obj.real_name == `${gatorName}`);
    
     document.querySelector('h3').innerText = cardDesc.real_name;
+    document.querySelector('img').src = 'https://arkhamDB.com' + cardDesc.imagesrc;
     document.querySelector('h4').innerText = cardDesc.type_name;    
     document.querySelector('h5').innerText = cardDesc.real_traits;
     document.querySelector('h6').innerText = cardDesc.real_text;
