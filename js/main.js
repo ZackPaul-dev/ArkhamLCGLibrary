26f1df4a8c736cd7e14361c0f9921b514f554608
Zack Paul <zkpaul@outlook.com>
2025-08-09 11:04:43 -0600

    begining to add styling

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ async function findGator(){
     let cardDesc = collection.find((obj) => obj.real_name.toLowerCase() == `${gatorName}`);
    
     document.querySelector('h3').innerText = cardDesc.real_name;
-    document.querySelector('img').src = 'https://arkhamDB.com' + cardDesc.imagesrc;
+    document.querySelector('.crdArt').src = 'https://arkhamDB.com' + cardDesc.imagesrc;
     document.querySelector('h4').innerText = cardDesc.type_name;    
     document.querySelector('h5').innerText = cardDesc.real_traits;
     document.querySelector('h6').innerText = cardDesc.real_text;
