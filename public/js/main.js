d8f9889365d7dcba4bb6ccac33ec782757df6667
Zack Paul <zkpaul@outlook.com>
2025-09-09 14:01:01 -0600

    building out architecture

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,5 @@
+const express = require('express');
+const router = express.Router;
+const homeController = require(.../controllers/home);
+
 document.querySelector('.sPage').addEventListener('click')
