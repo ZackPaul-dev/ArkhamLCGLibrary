5536ebc0a2c154b87da93890696757b315f260ff
Zack Paul <zkpaul@outlook.com>
2025-09-20 11:36:42 -0600

    routing

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const homeController = require('../controllers/home');
 
 router.get('/', homeController.getIndex)
