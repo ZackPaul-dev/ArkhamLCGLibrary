c73dc62446f12cd3eccbb5ed6d5676fb7764247d
Zack Paul <zkpaul@outlook.com>
2025-10-05 11:35:55 -0600

    routing

--- routes/search.js
+++ routes/search.js
@@ -1,32 +1,3 @@
-let collection = undefined;
-
-document.querySelector('.search').addEventListener('click', findGator)
-
-function getCards(req, res){
-
-        fetch('cards')
-        .then(res => res.json())
-        //.then(data => console.log(data))
-        .then(data => {
-   collection = data;
-   console.log(collection)
-});
-}
-getCards();
-
-/*async function testSearch(){
-    await(getCards);
-console.log(collection.find((obj) => obj.real_name == "Knife"));
-}*/
-
-async function findGator(){
-    await(getCards);
-    const gatorName = document.querySelector('input').value.toLowerCase();
-    let cardDesc = collection.find((obj) => obj.real_name.toLowerCase() == `${gatorName}`);
-   
-    document.querySelector('h3').innerText = cardDesc.real_name;
-    document.querySelector('.crdArt').src = 'https://arkhamDB.com' + cardDesc.imagesrc;
-    document.querySelector('h4').innerText = cardDesc.type_name;    
-    document.querySelector('h5').innerText = cardDesc.real_traits;
-    document.querySelector('h6').innerText = cardDesc.real_text;
-    } 
+const express = require('express');
+const router = express.Router();
+const searchController = require('../controllers/search')
